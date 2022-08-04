810a23f8f3d1409bf32d121dbf5fa99178015407
“Romer” <romeralvareziv@gmail.com>
2022-08-04 18:24:30 +0100

    optimization changes

--- js/index.js
+++ js/index.js
@@ -1,213 +1,203 @@
-// Pasos para crear este proyecto.
-// Primero necesitamos crear una baraja de cartas.
+const moduloBJ = (() => {
+    'use strict'
 
-/*
-2C = 2 puntos
-2H = 2 puntos
-2S = 2 puntos
-2D = 2 puntos
+    // Globales
 
-Especiales
+    let puntosJugadores = [];
+    let deck = [];
+    const tipos = ['C', 'D', 'H', 'S'],
+        especiales = ['J', 'Q', 'K', 'A'];
 
-*/
+    // Referencias al HTML
+    const smallPlayer = document.querySelectorAll('small'),
+     divCartasJugadores = document.querySelectorAll('.divCartas'),
+     btnPedir = document.querySelector('#btnPedir'),
+     btnDetener = document.querySelector('#btnDetener'),
+     btnNuevo = document.querySelector('#btnNuevo'),
+     divMensaje = document.querySelector('#mensaje');
 
-let puntosJugador = 0;
-let puntosComputadora = 0;
+    // Esta función inicializa el juego
+    const inicializarJuego = (numJugadores = 2)=>{
+        crearDeck();
 
-const smallPlayer = document.querySelectorAll('small');
-const divCartasJugador = document.querySelector('#jugador-cartas')
-const divCartasComputadora = document.querySelector('#computadora-cartas')
-const btnPedir = document.querySelector('#btnPedir')
-const btnDetener = document.querySelector('#btnDetener')
-const btnNuevo = document.querySelector('#btnNuevo')
-const divMensaje = document.querySelector('#mensaje')
+        puntosJugadores = [];
+        for(let i= 0; i < numJugadores; i++){
+            puntosJugadores.push(0);
+        }
 
+        smallPlayer.forEach( elem => elem.textContent = 0);
 
+        divMensaje.innerHTML = '';
+        divMensaje.classList.remove('mensaje-ganador')
+        divMensaje.classList.remove('mensaje-perdedor')
+        divMensaje.classList.remove('mensaje-empate')
+    
+        divCartasJugadores.forEach(elem => elem.innerHTML = '');
+    
+        btnDetener.disabled = false;
+        btnPedir.disabled = false;
+    }
+    
+    // Esta función crea una nueva baraja random
+    const crearDeck = () => {
 
-
-
-
-let deck = [];
-const tipos = ['C', 'D', 'H', 'S'];
-const especiales = ['J', 'Q', 'K', 'A'];
-
-
-// Esta función crea una nueva baraja random
-const crearDeck = () => {
-    for (let i = 2; i < 10; i++){
+        deck =[];
+        for (let i = 2; i < 10; i++){
+            for (let tipo of tipos){
+                deck.push(i + tipo);
+            }
+            
+        }
+    
         for (let tipo of tipos){
-            deck.push(i + tipo);
+            for(let especial of especiales){
+                deck.push(especial + tipo);
+            }
         }
-        
+
+        return deck.sort((a,b) => 0.5 - Math.random());
     }
 
-    for (let tipo of tipos){
-        for(let especial of especiales){
-            deck.push(especial + tipo);
+
+    // Esta función pide una carta
+    const pedirCarta = ()=>{
+        if(deck.length === 0){
+            throw('No hay cartas en el deck')
         }
+
+        return deck.pop();
+    }
+    
+    // Valor de la carta
+    const valorCarta = (carta) =>{
+        const valor = carta.substring(0, carta.length - 1);
+        return (isNaN(valor) ) ?
+                (valor === 'A') ? 11 : 10
+                : valor * 1;
+        // if(isNaN(valor)){
+        //     switch(carta){
+        //         case 'A': 
+        //             puntos += 10;
+        //             break;
+                    
+        //         default:
+        //             console.log('error')
+        //     }
+    
+        // } else{
+        //     puntos = valor * 1;
+        // }
+    }
+    
+    // Insertar mensajes de win/lose
+    const crearMensajeGanador = ()=>{
+        divMensaje.classList.add('mensaje-ganador')
+        const textoGanador = document.createElement('p')
+        textoGanador.classList.add('texto')
+        textoGanador.textContent = '¡Enhorabuena haz ganado!'
+        divMensaje.append(textoGanador);
+    }
+    
+    const crearMensajePerdedor = ()=>{
+        divMensaje.classList.add('mensaje-perdedor')
+        const textoPerdedor = document.createElement('p')
+        textoPerdedor.classList.add('texto')
+        textoPerdedor.textContent = 'Gana la consola, intentalo de nuevo...'
+        divMensaje.append(textoPerdedor);
+    }
+    
+    const crearMensajeEmpate = ()=>{
+        divMensaje.classList.add('mensaje-empate')
+        const textoEmpate = document.createElement('p')
+        textoEmpate.classList.add('texto')
+        textoEmpate.textContent = 'Ha habido un empate...'
+        divMensaje.append(textoEmpate);
     }
 
-    // console.log(deck)
-    shuffledDeck = deck.sort((a,b) => 0.5 - Math.random());
-    console.log(shuffledDeck)
-}
+    // turno: 0 primero jugador y el último es la computadora
+    const acumularPuntos = (carta, turno)=>{
+        puntosJugadores[turno] = puntosJugadores[turno] + valorCarta(carta)
+        smallPlayer[turno].textContent = puntosJugadores[turno];
 
-crearDeck();
+        return puntosJugadores[turno]
 
-// Esta función pide una carta
-const pedirCarta = ()=>{
-    if(deck.length === 0){
-        throw('No hay cartas en el deck')
     }
 
-    const carta = deck.pop();
-
-    return carta;
-}
-
-// Valor de la carta
-const valorCarta = (carta) =>{
-    const valor = carta.substring(0, carta.length - 1);
-    return (isNaN(valor) ) ?
-            (valor === 'A') ? 11 : 10
-            : valor * 1;
-    // if(isNaN(valor)){
-    //     switch(carta){
-    //         case 'A': 
-    //             puntos += 10;
-    //             break;
-                
-    //         default:
-    //             console.log('error')
-    //     }
-
-    // } else{
-    //     puntos = valor * 1;
-    // }
-}
-
-// Insertar mensajes de win/lose
-
-const crearMensajeGanador = ()=>{
-    divMensaje.classList.add('mensaje-ganador')
-    const textoGanador = document.createElement('p')
-    textoGanador.classList.add('texto')
-    textoGanador.textContent = '¡Enhorabuena haz ganado!'
-    divMensaje.append(textoGanador);
-}
-
-const crearMensajePerdedor = ()=>{
-    divMensaje.classList.add('mensaje-perdedor')
-    const textoPerdedor = document.createElement('p')
-    textoPerdedor.classList.add('texto')
-    textoPerdedor.textContent = 'Gana la consola, intentalo de nuevo...'
-    divMensaje.append(textoPerdedor);
-}
-
-const crearMensajeEmpate = ()=>{
-    divMensaje.classList.add('mensaje-empate')
-    const textoEmpate = document.createElement('p')
-    textoEmpate.classList.add('texto')
-    textoEmpate.textContent = 'Ha habido un empate...'
-    divMensaje.append(textoEmpate);
-}
-
-// Turno de la computadora
-
-const turnoComputadora = (puntosMinimos) =>{
-
-    do{
-
-        const carta = pedirCarta()
-        puntosComputadora = puntosComputadora+ valorCarta(carta)
-        smallPlayer[1].innerHTML = puntosComputadora
-    
-    
-        // Insertar carta
+    const crearCarta = (carta, turno) =>{
         const cartaHtml = document.createElement('img')
         cartaHtml.src = `/src/cartas/${carta}.png`
         cartaHtml.classList.add('card')
-        divCartasComputadora.append(cartaHtml)
-
-
-        if(puntosMinimos > 21){
-            break;
-        }
-    
-
-    }while( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) );
-
-    setTimeout(() => {
-        if(puntosComputadora === puntosMinimos){
-            crearMensajeEmpate()
-        } else if (puntosMinimos > 21){
-            crearMensajePerdedor()
-        } else if(puntosComputadora > 21){
-            crearMensajeGanador()
-        } else{
-            crearMensajePerdedor()
-        }
-    
-    }, 30);
-
-
-}
-
-// Eventos
-btnPedir.addEventListener('click', ()=>{
-    const carta = pedirCarta()
-    puntosJugador = puntosJugador  + valorCarta(carta)
-    smallPlayer[0].innerHTML = puntosJugador
-
-
-    // Insertar carta
-    const cartaHtml = document.createElement('img')
-    cartaHtml.src = `/src/cartas/${carta}.png`
-    cartaHtml.classList.add('card')
-    divCartasJugador.append(cartaHtml)
-
-
-    // Validación 
-    if(puntosJugador > 21){
-        console.warn('Haz perdido colega');
-        btnPedir.disabled = true;   
-        btnDetener.disabled = true;
-        turnoComputadora(puntosJugador)
-    } else if (puntosJugador === 21){
-        console.warn('21 Genial')
-        btnPedir.disabled = true;
-        btnDetener.disabled = true;
-        turnoComputadora(puntosJugador)
-
+        divCartasJugadores[turno].append(cartaHtml)
     }
-})
 
-btnDetener.addEventListener('click', ()=>{
-    btnDetener.disabled = true;
-    btnPedir.disabled = true;
-    turnoComputadora(puntosJugador)
-})
+    const determinarGanador = ()=>{
+        const [puntosMinimos, puntosComputadora] = puntosJugadores;
 
-btnNuevo.addEventListener('click', ()=>{
-    crearDeck();
-    puntosComputadora = 0;
-    puntosJugador = 0;
-
-    smallPlayer[0].innerHTML = 0;
-    smallPlayer[1].innerHTML = 0;
-
-    divMensaje.innerHTML = '';
-    divMensaje.classList.remove('mensaje-ganador')
-    divMensaje.classList.remove('mensaje-perdedor')
-    divMensaje.classList.remove('mensaje-empate')
-
-    divCartasJugador.innerHTML = '';
-    divCartasComputadora.innerHTML = '';
-
-    btnDetener.disabled = false;
-    btnPedir.disabled = false;
+        setTimeout(() => {
+            if(puntosComputadora === puntosMinimos){
+                crearMensajeEmpate()
+            } else if (puntosMinimos > 21){
+                crearMensajePerdedor()
+            } else if(puntosComputadora > 21){
+                crearMensajeGanador()
+            } else{
+                crearMensajePerdedor()
+            }
+        
+        }, 30);
+    }
     
-})
+    // Turno de la computadora
+    
+    const turnoComputadora = (puntosMinimos) =>{
+        let puntosComputadora = 0;
+    
+        do{
+    
+            const carta = pedirCarta()
+            puntosComputadora =  acumularPuntos(pedirCarta(), puntosJugadores.length - 1);
+            crearCarta(carta, puntosJugadores.length - 1);
+    
+        }while( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) );
 
+        determinarGanador();
+    }
+    
+    // Eventos
+    btnPedir.addEventListener('click', ()=>{
+        const carta = pedirCarta()
+        const puntosJugador = acumularPuntos(carta, 0)
 
+        crearCarta(carta, 0);
+    
+        // Validación 
+        if(puntosJugadores[0] > 21){
+            console.warn('Haz perdido colega');
+            btnPedir.disabled = true;   
+            btnDetener.disabled = true;
+            turnoComputadora(puntosJugador)
+        } else if (puntosJugadores[0] === 21){
+            console.warn('21 Genial')
+            btnPedir.disabled = true;
+            btnDetener.disabled = true;
+            turnoComputadora(puntosJugador)
+    
+        }
+    })
+    
+    btnDetener.addEventListener('click', ()=>{
+        btnDetener.disabled = true;
+        btnPedir.disabled = true;
+        turnoComputadora(puntosJugadores[0])
+    })
+    
+    btnNuevo.addEventListener('click', ()=>{
+        inicializarJuego()
+    })
+
+    return {
+        nuevoJuego: inicializarJuego
+    };
+        
+})(); 
 
--- readme.md
+++ readme.md
@@ -1,31 +1,50 @@
-# Simple Tailwind CSS Starter
+# Proyecto de Blackjack.
+Proyecto creado siguiendo el curso de JavaScript de Fernando Herrera en udemy.
 
-This is an extremley simple setup to develop Tailwind projects. This will be used in my Tailwind From Scratch course.
+## Primeros pasos a la hora de crear el proyecto:
+* 1. Primero debemos crear un deck de cartas que posteriormente lo vamos a mezclar. ¿Como hicimos esto?  
+    * Primero hemos inicializado una variable con un array vacío donde vamos a almacenar nuestras cartas.
+    Lo siguiente es inicializar una función y acto seguido, hemos creado dos array, uno con las cartas normales y luego uno con las cartas especiales y hemos creado dos bucles, uno que iteraremos del 2 al 9 (Número de cartas normales) y el siguiente hemos iterado todos los tipos normales por el número de cartas especiales y posteriormente inicializar esta función  
+    
 
-## Usage
+    ````js
+    let deck = [];
+    const tipos = ['C', 'D', 'H', 'S'];
+    const especiales = ['J', 'Q', 'K', 'A'];
 
-Install dependencies
 
-```
-npm install
-```
+    // Esta función crea una nueva baraja random
+    const crearDeck = () => {
+        for (let i = 2; i < 10; i++){
+            for (let tipo of tipos){
+                deck.push(i + tipo);
+            }
+        }
 
-Run Tailwind CLI in watch mode
+        for (let tipo of tipos){
+            for(let especial of especiales){
+                deck.push(especial + tipo);
+            }
+        }
 
-```
-npm run dev
-```
+        shuffledDeck = deck.sort((a,b) => 0.5 - Math.random());
+        
+        return shuffledDeck
+    }
 
-You can use tailwind classes in any .html files in the root directory
+    crearDeck();
+    ````
 
-Put any custom CSS that you may have in the **src/input.css** file
+* 2. El siguiente paso, debemos crear la función de pedir carta:
+    * Esto lo hemos hecho inicializando una función que elimina una carta del array, pero a su vez devuelve la carta que ha eliminado. Por otro lado, creamos una validación que tome los primeros dos elementos de la carta - 1.
+    ````js
+    const pedirCarta = ()=>{
+    if(deck.length === 0){
+        throw('No hay cartas en el deck')
+    }
 
-Add any config values to the **tailwind.config.js** file
+    const carta = deck.pop();
 
-To build once, run
-
-```
-npm run build
-```
-
-You only need to deploy your html files and css/style.css
+    return carta;
+    }
+    ````
